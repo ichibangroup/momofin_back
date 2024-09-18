9586506694f1efd52a6203700b002d80c4634f93
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-09-18 20:55:46 +0700

    Initial Commit

---
+++ src/main/java/ppl/momofin/momofinbackend/MomofinbackendApplication.java
@@ -0,0 +1,13 @@
+package ppl.momofin.momofinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomofinbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomofinbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ppl/momofin/momofinbackend/MomofinbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ppl.momofin.momofinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomofinbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
