c89c15e4bee50f8f25de65fda9f91862f6eab06f
Sirered <gikurniawan.1995@gmail.com>
2024-09-25 08:41:52 +0700

    [GREEN] Making the implementation for logging in

--- src/main/java/ppl/momofin/momofinbackend/controller/AuthController.java
+++ src/main/java/ppl/momofin/momofinbackend/controller/AuthController.java
@@ -1,4 +1,52 @@
 package ppl.momofin.momofinbackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ppl.momofin.momofinbackend.error.InvalidCredentialsException;
+import ppl.momofin.momofinbackend.model.User;
+import ppl.momofin.momofinbackend.service.UserService;
+import ppl.momofin.momofinbackend.utility.AuthRequest;
+import ppl.momofin.momofinbackend.utility.JwtUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody AuthRequest authRequest) {
+        try {
+            User authenticatedUser = userService.authenticate(
+                    authRequest.getOrganizationName(),
+                    authRequest.getEmail(),
+                    authRequest.getPassword()
+            );
+            String jwt = jwtUtil.generateToken(authenticatedUser.getEmail());
+
+            Map<String, Object> response = new HashMap<>();
+
+            response.put("jwt", jwt);
+            response.put("authenticatedUser", authenticatedUser);
+            return ResponseEntity.ok(response);
+        } catch (InvalidCredentialsException e) {
+            Map<String,String> response = new HashMap<>();
+
+            response.put("errorMessage", e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
 }
