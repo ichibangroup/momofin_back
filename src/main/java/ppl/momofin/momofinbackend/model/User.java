1b3f6c994c11db5dd2cb9e0b6b3e6027798b8e4d
Sirered <gikurniawan.1995@gmail.com>
2024-09-26 09:18:14 +0700

    [REFACTOR] Changed User table name

--- src/main/java/ppl/momofin/momofinbackend/model/User.java
+++ src/main/java/ppl/momofin/momofinbackend/model/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity @Getter @Setter
+@Table(name = "users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
