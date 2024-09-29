23fd164b3867e734e24a1d17dce1b45b6debc123
Sirered <gikurniawan.1995@gmail.com>
2024-09-29 18:26:48 +0700

    [GREEN] implemented Document model class

--- src/main/java/ppl/momofin/momofinbackend/model/Document.java
+++ src/main/java/ppl/momofin/momofinbackend/model/Document.java
@@ -1,4 +1,23 @@
 package ppl.momofin.momofinbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @Entity
 public class Document {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long documentId;
+    private String hashString;
+    private String name;
+
+    public Document() {}
+
+    public Document(String hashString, String name) {
+        this.hashString = hashString;
+        this.name = name;
+    }
 }
