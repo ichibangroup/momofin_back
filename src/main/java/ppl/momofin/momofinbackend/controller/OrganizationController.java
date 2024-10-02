892d94af8f4c0103cf4e8783d132e173f6ea5cd7
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-10-02 14:44:52 +0700

    [GREEN] Update OrganizationController

---
+++ src/main/java/ppl/momofin/momofinbackend/controller/OrganizationController.java
@@ -0,0 +1,25 @@
+package ppl.momofin.momofinbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ppl.momofin.momofinbackend.model.Organization;
+import ppl.momofin.momofinbackend.service.OrganizationService;
+
+@RestController
+@RequestMapping("/api/organizations")
+public class OrganizationController {
+
+    @Autowired
+    private OrganizationService organizationService;
+
+    @PutMapping("/{orgId}")
+    public ResponseEntity<Organization> updateOrganization(@PathVariable Long orgId, @RequestBody Organization organizationDetails) {
+        Organization updatedOrganization = organizationService.updateOrganization(
+                orgId,
+                organizationDetails.getName(),
+                organizationDetails.getDescription()
+        );
+        return ResponseEntity.ok(updatedOrganization);
+    }
+}
