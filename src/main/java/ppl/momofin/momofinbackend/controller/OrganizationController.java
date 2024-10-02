cd64baddc0336d94cdda3eb7c63bded91452fae4
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-10-02 14:53:31 +0700

    [REFACTOR] Apply RequireOrganizationAdmin to OrganizationController

--- src/main/java/ppl/momofin/momofinbackend/controller/OrganizationController.java
+++ src/main/java/ppl/momofin/momofinbackend/controller/OrganizationController.java
@@ -3,6 +3,7 @@ package ppl.momofin.momofinbackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ppl.momofin.momofinbackend.annotation.RequireOrganizationAdmin;
 import ppl.momofin.momofinbackend.model.Organization;
 import ppl.momofin.momofinbackend.service.OrganizationService;
 
@@ -13,6 +14,7 @@ public class OrganizationController {
     @Autowired
     private OrganizationService organizationService;
 
+    @RequireOrganizationAdmin
     @PutMapping("/{orgId}")
     public ResponseEntity<Organization> updateOrganization(@PathVariable Long orgId, @RequestBody Organization organizationDetails) {
         Organization updatedOrganization = organizationService.updateOrganization(
