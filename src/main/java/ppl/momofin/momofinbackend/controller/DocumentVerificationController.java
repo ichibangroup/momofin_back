32c2858538369269ec493ceeebf5314822582972
mariagrizelda <mariahelga423@gmail.com>
2024-10-03 21:27:17 +0700

    [GREEN] add implementations for get users list of uploaded documents

--- src/main/java/ppl/momofin/momofinbackend/controller/DocumentVerificationController.java
+++ src/main/java/ppl/momofin/momofinbackend/controller/DocumentVerificationController.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import ppl.momofin.momofinbackend.response.DocumentSubmissionSuccessResponse;
-import ppl.momofin.momofinbackend.response.DocumentVerificationSuccessResponse;
-import ppl.momofin.momofinbackend.response.ErrorResponse;
-import ppl.momofin.momofinbackend.response.Response;
+import ppl.momofin.momofinbackend.model.User;
+import ppl.momofin.momofinbackend.response.*;
 import ppl.momofin.momofinbackend.security.JwtUtil;
 import ppl.momofin.momofinbackend.service.DocumentService;
 import ppl.momofin.momofinbackend.model.Document;
+import ppl.momofin.momofinbackend.service.UserService;
+import java.util.List;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
@@ -21,11 +21,13 @@ import java.security.NoSuchAlgorithmException;
 public class DocumentVerificationController {
     private final DocumentService documentService;
     private final JwtUtil jwtUtil;
+    private final UserService userService;
 
     @Autowired
-    public DocumentVerificationController(DocumentService documentService, JwtUtil jwtUtil) {
+    public DocumentVerificationController(DocumentService documentService, JwtUtil jwtUtil, UserService userService) {
         this.documentService = documentService;
         this.jwtUtil = jwtUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/submit")
@@ -52,6 +54,18 @@ public class DocumentVerificationController {
             ErrorResponse errorResponse = new ErrorResponse("Error verifying document: " + e.getMessage());
             return ResponseEntity.badRequest().body(errorResponse);
         }
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<Response> getAllUsersDocument(@RequestHeader("Authorization") String token) {
+        String username = getUsername(token,jwtUtil);
+
+        User user = userService.fetchUserByUsername(username);
+        List<Document> documents = documentService.findAllDocumentsByOwner(user);
+
+        UserDocumentsResponse userDocumentsResponse = new UserDocumentsResponse(user, documents);
+
+        return ResponseEntity.ok(userDocumentsResponse);
     }
 
     public static String getUsername(String token, JwtUtil jwtUtil) {
