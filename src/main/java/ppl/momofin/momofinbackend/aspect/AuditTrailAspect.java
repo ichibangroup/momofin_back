844409fb7ad9fa90cb22cb95a35fe4caa1481d58
cl4ytn <c.nagle744@gmail.com>
2024-10-17 22:36:50 +0700

    [REFACTOR] audit trail aspect

--- src/main/java/ppl/momofin/momofinbackend/aspect/AuditTrailAspect.java
+++ src/main/java/ppl/momofin/momofinbackend/aspect/AuditTrailAspect.java
@@ -1,8 +1,8 @@
 package ppl.momofin.momofinbackend.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 import ppl.momofin.momofinbackend.model.AuditTrail;
 import ppl.momofin.momofinbackend.model.Document;
 import ppl.momofin.momofinbackend.model.User;
-import ppl.momofin.momofinbackend.service.UserService;
 import ppl.momofin.momofinbackend.repository.AuditTrailRepository;
+import ppl.momofin.momofinbackend.service.UserService;
 
 @Aspect
 @Component
@@ -22,28 +22,48 @@ public class AuditTrailAspect {
     @Autowired
     private AuditTrailRepository auditTrailRepository;
 
-    public AuditTrailAspect(UserService userService) {
+    @Autowired
+    public AuditTrailAspect(UserService userService, AuditTrailRepository auditTrailRepository) {
         this.userService = userService;
+        this.auditTrailRepository = auditTrailRepository;
     }
 
-    @AfterReturning(pointcut = "execution(* ppl.momofin.momofinbackend.service.DocumentService.verifyDocument(..))", returning = "document")
-    public void logDocumentVerifyReturn(JoinPoint joinPoint, Document document) {
-        logActivity(joinPoint, document, "VERIFY");
-    }
+    @Pointcut("execution(* ppl.momofin.momofinbackend.service.CDNService.uploadFile(..))")
+    public void documentSubmitPointcut() {}
 
-    private void logActivity(JoinPoint joinPoint, Document document, String action) {
+    @Pointcut("execution(* ppl.momofin.momofinbackend.service.DocumentService.verifyDocument(..))")
+    public void documentVerifyPointcut() {}
+
+    @Pointcut("documentSubmitPointcut() || documentVerifyPointcut()")
+    public void documentUploadPointcut() {}
+
+    @AfterReturning(pointcut = "documentUploadPointcut()", returning = "document")
+    public void captureDocumentAfterUpload(Document document) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = null;
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            username = authentication.getName();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            System.out.println("User is not authenticated, audit trail not saved.");
+            return;
         }
 
+        String username = authentication.getName();
         User user = userService.fetchUserByUsername(username);
+
+        if (document == null) {
+            System.out.println("Document not found, audit trail not saved.");
+            return;
+        }
+
+        if (user == null) {
+            System.out.println("User not found, audit trail not saved.");
+            return;
+        }
+
         AuditTrail auditTrail = new AuditTrail();
         auditTrail.setUser(user);
-        auditTrail.setAction(action);
+        auditTrail.setAction("pending");
         auditTrail.setDocument(document);
-        auditTrail.setAuditOutcome(document != null ? "SUCCESS" : "FAILED");
+        auditTrail.setAuditOutcome("SUCCESS");
 
         auditTrailRepository.save(auditTrail);
     }
