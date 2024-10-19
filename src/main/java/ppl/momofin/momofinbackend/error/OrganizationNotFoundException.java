aa4e6d7bf95b9c8db4ed7b822362ef494dbe02b1
Sirered <gikurniawan.1995@gmail.com>
2024-10-19 18:43:03 +0700

    [GREEN] Creating implementation for fetching all users and getting organization for frontend

--- src/main/java/ppl/momofin/momofinbackend/error/OrganizationNotFoundException.java
+++ src/main/java/ppl/momofin/momofinbackend/error/OrganizationNotFoundException.java
@@ -4,4 +4,8 @@ public class OrganizationNotFoundException extends RuntimeException{
     public OrganizationNotFoundException(String organizationName) {
         super("The organization "+ organizationName + " is not registered to our database");
     }
+
+    public OrganizationNotFoundException() {
+        super("Organization not found");
+    }
 }
