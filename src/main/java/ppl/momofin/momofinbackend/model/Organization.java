eef01df210cf0fd38f17dd120961a5ac44e6c3d9
Sirered <gikurniawan.1995@gmail.com>
2024-10-20 21:52:04 +0700

    [REFACTOR] Resolving maintainability code smells

--- src/main/java/ppl/momofin/momofinbackend/model/Organization.java
+++ src/main/java/ppl/momofin/momofinbackend/model/Organization.java
@@ -1,7 +1,6 @@
 package ppl.momofin.momofinbackend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
--- src/test/java/ppl/momofin/momofinbackend/model/OrganizationTest.java
+++ src/test/java/ppl/momofin/momofinbackend/model/OrganizationTest.java
@@ -1,6 +1,5 @@
 package ppl.momofin.momofinbackend.model;
 
-import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
