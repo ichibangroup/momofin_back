17171a0927e33361dea42a99af062089e8769d1c
cl4ytn <c.nagle744@gmail.com>
2024-10-21 09:06:11 +0700

    [FIX] Deleted a public modifier

--- src/test/java/ppl/momofin/momofinbackend/controller/AuditTrailControllerTest.java
+++ src/test/java/ppl/momofin/momofinbackend/controller/AuditTrailControllerTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class AuditTrailControllerTest {
+class AuditTrailControllerTest {
 
     @Mock
     private AuditTrailService auditTrailService;
