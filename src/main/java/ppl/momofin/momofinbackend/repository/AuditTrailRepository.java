ee6df950ea7949700875363203688b1b711c40e8
cl4ytn <c.nagle744@gmail.com>
2024-10-21 08:30:51 +0700

    [REFACTOR] Removed useless method

--- src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
@@ -3,6 +3,7 @@ package ppl.momofin.momofinbackend.repository;
 import ppl.momofin.momofinbackend.model.AuditTrail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ppl.momofin.momofinbackend.model.User;
 
 import java.util.List;
 
