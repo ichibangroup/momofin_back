a4ca69af48bb2b8b50917610cc9808200a0bb9aa
cl4ytn <c.nagle744@gmail.com>
2024-11-01 06:23:45 +0700

    [REFACTOR] AuditTrailRepo for pagination

--- src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
@@ -3,11 +3,11 @@ package ppl.momofin.momofinbackend.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ppl.momofin.momofinbackend.model.AuditTrail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long> {
-    Page<AuditTrail> findAll(Specification<AuditTrail> spec, Pageable pageable);
+public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long>, JpaSpecificationExecutor<AuditTrail> {
 }
