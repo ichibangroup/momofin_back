cf897a2f55033260c7e915aafc74490f2dad3125
cl4ytn <c.nagle744@gmail.com>
2024-10-31 22:35:34 +0700

    [REFACTOR] Added findAll method to AuditTrailRepo

--- src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
@@ -1,8 +1,13 @@
 package ppl.momofin.momofinbackend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import ppl.momofin.momofinbackend.model.AuditTrail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long> { }
+public interface AuditTrailRepository extends JpaRepository<AuditTrail, Long> {
+    Page<AuditTrail> findAll(Specification<AuditTrail> spec, Pageable pageable);
+}
