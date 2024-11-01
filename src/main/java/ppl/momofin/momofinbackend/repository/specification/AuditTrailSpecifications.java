935394297276a4117e8b3250118db64bbdef157a
cl4ytn <c.nagle744@gmail.com>
2024-11-01 08:08:06 +0700

    [REFACTOR] AuditTrailSpecifications added Time Filters

--- src/main/java/ppl/momofin/momofinbackend/repository/specification/AuditTrailSpecifications.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/specification/AuditTrailSpecifications.java
@@ -2,6 +2,9 @@ package ppl.momofin.momofinbackend.repository.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 import ppl.momofin.momofinbackend.model.AuditTrail;
+import ppl.momofin.momofinbackend.model.User;
+
+import java.time.LocalDateTime;
 
 public class AuditTrailSpecifications {
 
@@ -9,7 +12,16 @@ public class AuditTrailSpecifications {
         return (root, query, cb) -> cb.equal(root.get("action"), action);
     }
 
-    public static Specification<AuditTrail> hasUser(String user) {
+    public static Specification<AuditTrail> hasUser(User user) {
         return (root, query, cb) -> cb.equal(root.get("user"), user);
     }
+
+    public static Specification<AuditTrail> afterTimestamp(LocalDateTime startDateTime) {
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("timestamp"), startDateTime);
+    }
+
+    public static Specification<AuditTrail> beforeTimestamp(LocalDateTime endDateTime) {
+        return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("timestamp"), endDateTime);
+    }
+
 }
