992a2f5d27d938f558acf2cbd95235618195c44b
cl4ytn <c.nagle744@gmail.com>
2024-11-12 22:22:31 +0700

    [REFACTOR] Added more robust error handling

--- src/main/java/ppl/momofin/momofinbackend/repository/specification/AuditTrailSpecifications.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/specification/AuditTrailSpecifications.java
@@ -2,26 +2,33 @@ package ppl.momofin.momofinbackend.repository.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 import ppl.momofin.momofinbackend.model.AuditTrail;
-import ppl.momofin.momofinbackend.model.User;
 
 import java.time.LocalDateTime;
 
 public class AuditTrailSpecifications {
 
     public static Specification<AuditTrail> hasAction(String action) {
-        return (root, query, cb) -> cb.equal(root.get("action"), action);
+        return (root, query, cb) ->
+                action == null ? cb.conjunction() : cb.equal(root.get("action"), action);
     }
 
-    public static Specification<AuditTrail> hasUser(User user) {
-        return (root, query, cb) -> cb.equal(root.get("user"), user);
+    public static Specification<AuditTrail> hasUser(String username) {
+        return (root, query, cb) ->
+                username == null ? cb.conjunction() : cb.like(cb.lower(root.get("user").get("username")), "%" + username.toLowerCase() + "%");
     }
 
     public static Specification<AuditTrail> afterTimestamp(LocalDateTime startDateTime) {
-        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get("timestamp"), startDateTime);
+        return (root, query, cb) ->
+                startDateTime == null ? cb.conjunction() : cb.greaterThanOrEqualTo(root.get("timestamp"), startDateTime);
     }
 
     public static Specification<AuditTrail> beforeTimestamp(LocalDateTime endDateTime) {
-        return (root, query, cb) -> cb.lessThanOrEqualTo(root.get("timestamp"), endDateTime);
+        return (root, query, cb) ->
+                endDateTime == null ? cb.conjunction() : cb.lessThanOrEqualTo(root.get("timestamp"), endDateTime);
     }
 
+    public static Specification<AuditTrail> hasDocumentName(String documentName) {
+        return (root, query, cb) ->
+                documentName == null ? cb.conjunction() : cb.like(cb.lower(root.get("document").get("name")), "%" + documentName.toLowerCase() + "%");
+    }
 }
