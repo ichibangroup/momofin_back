60b81ea5532eec01e20a1bfa05ce310b25256d8b
Sirered <gikurniawan.1995@gmail.com>
2024-11-22 01:49:19 +0700

    configuring prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation("com.google.cloud:google-cloud-storage:$gcpVersion")
     implementation("io.sentry:sentry:$sentryVersion")
     implementation("io.sentry:sentry-spring-boot-starter:$sentryVersion")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
