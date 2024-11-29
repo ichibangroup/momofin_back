4629854eacd47fca19eb780f70246f455329d706
cl4ytn <c.nagle744@gmail.com>
2024-11-29 14:30:32 +0800

    [REFACTOR] Delete unused import

--- src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
+++ src/main/java/ppl/momofin/momofinbackend/repository/AuditTrailRepository.java
@@ -1,8 +1,5 @@
 package ppl.momofin.momofinbackend.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ppl.momofin.momofinbackend.model.AuditTrail;
 import org.springframework.data.jpa.repository.JpaRepository;
